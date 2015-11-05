ec078ca9fc44007c4fa6b090cb33cef6677d530c
austin ezell <austin.r.ezell@gmail.com>
2015-11-04 19:57:01 -0800

    succesfully gathers blurbs

--- blurb.js
+++ blurb.js
@@ -3,26 +3,26 @@
 let fs = require('fs');
 let request = require('request');
 let cheerio = require('cheerio');
-let newData = [];
+let newData = {landmarks: []};
 let data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
-data.landmarks = data.landmarks.splice(0, 25)
-{
-  data = data.landmarks.map( (landmark, i) => {
-    let url = landmark.wikiLink
-      function getBlurb () {
-        request(url, (err, response, html)=>{
-        if(!err){
-          let $ = cheerio.load(html)
-          let blurb = $('#mw-content-text>p').first().text()
-          blurb = blurb.replace(/\[\d+\]/g, "")
-          landmark.blurb = blurb
-          return landmark
-        }
-      })
-    }
-    return getBlurb()
+let writeFile = () => {
+  fs.writeFile('blurbs.json', JSON.stringify(newData), function(err){
+    console.log('hit');
   })
 }
-fs.writeFile('blurbs.json', JSON.stringify(data), function(err){
-  console.log('hit');
+data.landmarks.forEach( (landmark, i) => {
+  let url = landmark.wikiLink
+  request(url, (err, response, html)=>{
+    if(!err){
+      let $ = cheerio.load(html)
+      let blurb = $('#mw-content-text>p').first().text()
+      blurb = blurb.replace(/\[\d+\]/g, "")
+      landmark.blurb = blurb
+      newData.landmarks.push(landmark)
+      console.log(newData.landmarks.length, data.landmarks.length);
+      if(newData.landmarks.length === data.landmarks.length){
+        writeFile()
+      }
+    }
+  })
 })
